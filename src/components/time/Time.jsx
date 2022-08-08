579882f0ec32b5e0830e3e71dc39092cd6d52c87
GitBartbe <Bartosz.bednarski@wp.pl>
2022-08-08 16:30:22 +0200

    changed order of date

--- src/components/time/Time.jsx
+++ src/components/time/Time.jsx
@@ -1,14 +1,14 @@
-import { format, parseISO } from 'date-fns';
+import { format, parseISO } from "date-fns";
 
-const Time = ({ date = '2022-07-07T16:19:28.495Z', displayDate }) => {
+const Time = ({ date = "2022-07-07T16:19:28.495Z", displayDate }) => {
   const parsedDate = parseISO(date);
 
-  const formatedDate = format(parsedDate, 'MM.dd.yyyy');
-  const formatHour = format(parsedDate, 'hh:mm aa');
+  const formatedDate = format(parsedDate, "dd.mm.yyyy");
+  const formatHour = format(parsedDate, "hh:mm aa");
 
   return (
     <div>
-      <div className='note-data'>
+      <div className="note-data">
         {displayDate && <h4>{formatedDate}</h4>}
 
         <h4>{formatHour}</h4>
